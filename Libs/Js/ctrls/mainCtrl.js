932cb276d4cc6f3eb4475b3227c18e8e545de79b
dreamofei <jiangyafeinet@126.com>
2015-12-10 21:21:18 +0800

    2015.12.10_2

--- Libs/Js/ctrls/mainCtrl.js
+++ Libs/Js/ctrls/mainCtrl.js
@@ -1,5 +1,87 @@
 ﻿mainMoudle.controller('slideImgCtrl', ['$scope', function ($scope) {
 
+    $scope.slideImgs = [
+        { img: "/Sources/imgs/slideImg/01.jpg" },
+        { img: "/Sources/imgs/slideImg/02.jpg" },
+        { img: "/Sources/imgs/slideImg/03.jpg" },
+        { img: "/Sources/imgs/slideImg/04.jpg" },
+        { img: "/Sources/imgs/slideImg/05.jpg" }
+    ];
+
+    //slideImg
+    var $ = function (id) { return typeof id === "string" ? document.getElementById(id) : id };
+    var $$ = function (tagName, oParent) { return (oParent || document).getElementsByTagName(tagName) };
+    //自动播放对象
+    var AutoPlay = function (id) { this.initialize(id) };
+    AutoPlay.prototype = {
+        initialize: function (id) {
+            var oThis = this;
+            this.oBox = $(id);
+            this.oUl = $$("ul", this.oBox)[0];
+            this.aImg = $$("img", this.oBox);
+            this.timer = null;
+            this.autoTimer = null;
+            this.iNow = 0;
+            this.creatBtn();
+            this.aBtn = $$("li", this.oCount);
+            this.toggle();
+            this.autoTimer = setInterval(function () {
+                oThis.next()
+            }, 3000);
+            this.oBox.onmouseover = function () {
+                clearInterval(oThis.autoTimer)
+            };
+            this.oBox.onmouseout = function () {
+                oThis.autoTimer = setInterval(function () {
+                    oThis.next()
+                }, 3000)
+            };
+            for (var i = 0; i < this.aBtn.length; i++) {
+                this.aBtn[i].index = i;
+                this.aBtn[i].onmouseover = function () {
+                    oThis.iNow = this.index;
+                    oThis.toggle()
+                }
+            }
+        },
+        creatBtn: function () {
+            this.oCount = document.createElement("ul");
+            this.oFrag = document.createDocumentFragment();
+            this.oCount.className = "count";
+            for (var i = 0; i < this.aImg.length; i++) {
+                var oLi = document.createElement("li");
+                oLi.innerHTML = i + 1;
+                this.oFrag.appendChild(oLi)
+            }
+            this.oCount.appendChild(this.oFrag);
+            this.oBox.appendChild(this.oCount)
+        },
+        toggle: function () {
+            for (var i = 0; i < this.aBtn.length; i++) this.aBtn[i].className = "";
+            this.aBtn[this.iNow].className = "current";
+            //this.doMove(-(this.iNow * this.aImg[0].offsetHeight))
+            this.doMove(-(this.iNow * this.aImg[0].offsetWidth))
+        },
+        next: function () {
+            this.iNow++;
+            this.iNow == this.aBtn.length && (this.iNow = 0);
+            this.toggle()
+        },
+        doMove: function (iTarget) {
+            var oThis = this;
+            clearInterval(oThis.timer);
+            oThis.timer = setInterval(function () {
+                var iSpeed = (iTarget - oThis.oUl.offsetLeft) / 5;
+                iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+                oThis.oUl.offsetLeft == iTarget ? clearInterval(oThis.timer) : (oThis.oUl.style.left = oThis.oUl.offsetLeft + iSpeed + "px")
+            }, 30)
+        }
+    };
+    $scope.setScroll = function () {
+        new AutoPlay("box_yingxinslide");
+    };
+    //end slideImg
+
 }])
 .controller('sideMenuCtrl', function ($scope) {
     $scope.sideMenu = [
