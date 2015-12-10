f96db6d6987b763fb754fd02e3952d85d447d146
dreamofei <jiangyafeinet@126.com>
2015-12-10 19:18:50 +0800

    2015.12.10

--- Libs/Js/ctrls/mainCtrl.js
+++ Libs/Js/ctrls/mainCtrl.js
@@ -4,33 +4,33 @@
 .controller('sideMenuCtrl', function ($scope) {
     $scope.sideMenu = [
         {
-            name: "脉冲电子围栏", url: "#", child: [
-                { name: "脉冲控制器", url: "#", img: "/Sources/imgs/R1.png" },
-                { name: "围栏配件", url: "#", img: "/Sources/imgs/R1.png" }
+            name: "脉冲电子围栏", url: "#/", child: [
+                { name: "脉冲控制器", url: "#/", img: "/Sources/imgs/R1.png" },
+                { name: "围栏配件", url: "#/", img: "/Sources/imgs/R1.png" }
             ], isFocus: false
         },
         {
-            name: "张力电子围栏", url: "#", child: [
-                { name: "张力探测器", url: "#", img: "/Sources/imgs/R1.png" },
-                { name: "围栏配件", url: "#", img: "/Sources/imgs/R1.png" }
+            name: "张力电子围栏", url: "#/", child: [
+                { name: "张力探测器", url: "#/", img: "/Sources/imgs/R1.png" },
+                { name: "围栏配件", url: "#/", img: "/Sources/imgs/R1.png" }
             ], isFocus: false
         },
         {
-            name: "智能管理中心", url: "#", child: [
-                { name: "智能报警主机", url: "#", img: "/Sources/imgs/R1.png" },
-                { name: "主机配套设备", url: "#", img: "/Sources/imgs/R1.png" }
+            name: "智能管理中心", url: "#/", child: [
+                { name: "智能报警主机", url: "#/", img: "/Sources/imgs/R1.png" },
+                { name: "主机配套设备", url: "#/", img: "/Sources/imgs/R1.png" }
             ], isFocus: false
         },
         {
-            name: "振动光纤探测器", url: "#", child: [
-                { name: "双防区探测控制器", url: "#", img: "/Sources/imgs/R1.png" },
-                { name: "八防区光纤探测控制器", url: "#", img: "/Sources/imgs/R1.png" }
+            name: "振动光纤探测器", url: "#/", child: [
+                { name: "双防区探测控制器", url: "#/", img: "/Sources/imgs/R1.png" },
+                { name: "八防区光纤探测控制器", url: "#/", img: "/Sources/imgs/R1.png" }
             ], isFocus: false
         },
         {
-            name: "高清网络光端机", url: "#", child: [
-                { name: "100M（百兆）高清系列", url: "#", img: "/Sources/imgs/R1.png" },
-                { name: "1000M（千兆）高清系列", url: "#", img: "/Sources/imgs/R1.png" }
+            name: "高清网络光端机", url: "#/", child: [
+                { name: "100M（百兆）高清系列", url: "#/", img: "/Sources/imgs/R1.png" },
+                { name: "1000M（千兆）高清系列", url: "#/", img: "/Sources/imgs/R1.png" }
             ], isFocus: false
         }
     ];
@@ -44,22 +44,20 @@
 })
 .controller("classicalCaseCtrl", function ($scope) {
     $scope.scrollImgs = [
-        { name: "test001", url: "#", img: "/Sources/imgs/01.jpg" },
-        { name: "test002", url: "#", img: "/Sources/imgs/02.jpg" },
-        { name: "test003", url: "#", img: "/Sources/imgs/03.jpg" },
-        { name: "test004", url: "#", img: "/Sources/imgs/04.jpg" },
-        { name: "test005", url: "#", img: "/Sources/imgs/05.jpg" },
-        { name: "test006", url: "#", img: "/Sources/imgs/06.jpg" }
+        { name: "test001", url: "#/", img: "/Sources/imgs/01.jpg" },
+        { name: "test002", url: "#/", img: "/Sources/imgs/02.jpg" },
+        { name: "test003", url: "#/", img: "/Sources/imgs/03.jpg" },
+        { name: "test004", url: "#/", img: "/Sources/imgs/04.jpg" },
+        { name: "test005", url: "#/", img: "/Sources/imgs/05.jpg" },
+        { name: "test006", url: "#/", img: "/Sources/imgs/06.jpg" }
     ];
 
-    window.onload = function () {
-        var speed = 20;
+    $scope.setScroll = function () {
+        var speed = 15;
         var container = document.getElementsByClassName("scrollContainer")[0];
-        //var container = document.getElementsByClassName("scrollContainer-inner")[0];
         var imgsDiv = document.getElementsByClassName("scrollContainer-imgs");
         var imgdiv1 = imgsDiv[0];
         var imgdiv2 = imgsDiv[1];
-        //imgdiv2.innerHTML = imgdiv1.innerHTML;
         var Scrolling = function () {
             if (imgdiv2.offsetWidth - container.scrollLeft <= 0)
                 container.scrollLeft -= imgdiv1.offsetWidth
@@ -70,21 +68,19 @@
         var scrollInterval = setInterval(Scrolling, speed);
         container.onmouseover = function () { clearInterval(scrollInterval) };
         container.onmouseout = function () { scrollInterval = setInterval(Scrolling, speed) };
-
-        //$('[data-toggle="tooltip"]').tooltip({ placement: "bottom" });
     }
 
 })
 .controller('QANewsCtrl', function ($scope) {
     //news
     $scope.news = [
-        { content: "新闻一。。。。", url: "#", isFocus: false },
-        { content: "新闻eer。。。。", url: "#", isFocus: false },
-        { content: "sdfdsfsdfd。。。。", url: "#", isFocus: false },
-        { content: "新闻一ddfasdf", url: "#", isFocus: false },
-        { content: "sdfsdfsd。。。。", url: "#", isFocus: false },
-        { content: "ghtrtuyrr。。。。", url: "#", isFocus: false },
-        { content: "htre。。。。", url: "#", isFocus: false }
+        { content: "新闻一。。。。", url: "#/", isFocus: false },
+        { content: "新闻eer。。。。", url: "#/", isFocus: false },
+        { content: "sdfdsfsdfd。。。。", url: "#/", isFocus: false },
+        { content: "新闻一ddfasdf", url: "#/", isFocus: false },
+        { content: "sdfsdfsd。。。。", url: "#/", isFocus: false },
+        { content: "ghtrtuyrr。。。。", url: "#/", isFocus: false },
+        { content: "htre。。。。", url: "#/", isFocus: false }
     ]
     $scope.aLinkHover = function (index) {
         $scope.news[index].isFocus = true;
@@ -99,32 +95,32 @@
         {
             question: "了空间了空间浪费空间？",
             answer: "了空间链接了解了了时间段里疯狂就死定了拉开距离龙聚楼酒楼。",
-            url: "#"
+            url: "#/"
         },
         {
             question: "欧赔，门将离开了我我我？",
             answer: "路肩路肩了解空间抛弃破IPIQ颇为家里快急死了到家了抛弃破费破量减少了空间刻录机你，没呢，吗。",
-            url: "#"
+            url: "#/"
         },
         {
             question: "户名称来看及？",
             answer: "我我我我ijoiiouosuf看见了看见了看见了健康了健康记录劈破。",
-            url: "#"
+            url: "#/"
         },
         {
             question: "品牌空间关闭接口卡啦啦啦？",
             answer: "哦IE偶奇偶及皮肤鹿鼎记佛经里看见的怕看见了家里快乐偶奇偶及皮肤鹿鼎记佛经里看见的怕看见了家里快乐偶奇偶及皮肤鹿鼎记佛经里看见的怕看见了家里快乐。",
-            url: "#"
+            url: "#/"
         },
         {
             question: "两句话给帮忙看看匹配进口零距离？",
             answer: "我看见了建瓯盘欺骗记录到快来看才看见哦自己哦了。",
-            url: "#"
+            url: "#/"
         }
     ];
 
-    window.onload = function () {
-        var speed = 20;
+    $scope.setScroll = function () {
+        var speed = 30;
         var qaScroll = document.getElementById("qaScroll");
         var qaScroll1 = document.getElementById("qaScroll1");
         var qaScroll2 = document.getElementById("qaScroll2");
@@ -139,7 +135,6 @@
         qaScroll.onmouseover = function () { clearInterval(scrollInterval) };
         qaScroll.onmouseout = function () { scrollInterval = setInterval(Scrolling, speed) };
 
-        //$('[data-toggle="tooltip"]').tooltip({ placement: "bottom" });
     }
     //end qa
 })
---
+++ Libs/Js/directives/directives.js
@@ -0,0 +1,15 @@
+﻿mainMoudle.directive('onFinishRender', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last == true) {
+                $timeout(function () {
+                    var fn = attr.onFinishRender;
+                    scope.$eval(fn);
+                });
+            }
+        }
+    };
+})
+
+;
